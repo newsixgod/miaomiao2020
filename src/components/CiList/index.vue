da86a9cf0d7f869388cbaeed614acdc4f486930d
newsixgod <1101128058@qq.com>
2020-04-25 10:30:30 +0800

    add setData

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,104 +1,78 @@
 <template>
-    <div id="content">
-        <div class="cinema_body">
-            <ul>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+    <div class="cinema_body">
+        <ul>
+            <li v-for="item in cinemaList" :key="item.id">
+                <div>
+                    <span>{{ item.nm }}</span>
+                    <span class="q"><span class="price">{{ item.sellPrice }}</span> 元起</span>
+                </div>
+                <div class="address">
+                    <span>{{ item.addr }}</span>
+                    <span>{{ item.distance }}</span>
+                </div>
+                <div class="card">
+                    <div v-for="(num,key) in item.tag" v-if="num===1" :key="key" :class=" key | classCard ">{{ key | formatCard }}</div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name:'CiList'
+    name : 'CiList',
+    data(){
+        return {
+            cinemaList : [],
+        };
+    },
+    activated(){
+
+        this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
+            var msg = res.data.msg;
+            if(msg === 'ok'){
+                this.cinemaList = res.data.data.cinemas;
+            }
+        });
+    },
+    filters : {
+        // 过滤器 后台传回来 num 是  0 1 0 1   这里有 1 的就匹配显示   看前面  v-if="num===1"
+        // 后台传回来 allowRefund  endorse 等是英文，这里过滤器显示相应的中文，而且是 1 的就显示
+        formatCard(key){
+            var card = [
+                { key : 'allowRefund' , value : '改签' },
+                { key : 'endorse' , value : '退' },
+                { key : 'sell' , value : '折扣卡' },
+                { key : 'snack' , value : '小吃'}
+            ];
+            for(var i=0;i<card.length;i++){
+                if(card[i].key === key){
+                    return card[i].value;
+                }
+            }
+            return '';
+        },
+        // 过滤器 显示的小标签给个颜色 后台传回来 num 是  0 1 0 1   这里有 1 的就匹配显示   看前面  v-if="num===1"
+        // 后台传回来 allowRefund  endorse 等是英文，这里过滤器显示相应的中文，而且是 1 的就显示
+        classCard(key){
+            var card = [
+                { key : 'allowRefund' , value : 'bl' },
+                { key : 'endorse' , value : 'bl' },
+                { key : 'sell' , value : 'or' },
+                { key : 'snack' , value : 'or'}
+            ];
+            for(var i=0;i<card.length;i++){
+                if(card[i].key === key){
+                    return card[i].value;
+                }
+            }
+            return '';
+        }
+    }
 }
 </script>
 
-<style scoped >
-
+<style scoped>
     #content .cinema_body{ flex:1; overflow:auto;}
     .cinema_body ul{ padding:20px;}
     .cinema_body li{  border-bottom:1px solid #e6e6e6; margin-bottom: 20px;}
@@ -111,5 +85,4 @@ export default {
     .cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90; font-size: 13px; margin-right: 5px;}
     .cinema_body .card div.or{ color: #f90; border: 1px solid #f90;}
     .cinema_body .card div.bl{ color: #589daf; border: 1px solid #589daf;}
-
 </style>
--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,97 +1,127 @@
 <template>
-  <div id="content">
     <div class="city_body">
         <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
                 <ul class="clearfix">
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
+                    <li v-for="item in hotList" :key="item.id">{{ item.nm }}</li>
                 </ul>
             </div>
-            <div class="city_sort">
-                <div>
-                    <h2>A</h2>
+            <div class="city_sort" ref="city_sort">
+                <div v-for="item in cityList" :key="item.index">
+                    <h2>{{ item.index }}</h2>
                     <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
+                        <li v-for="itemList in item.list" :key="itemList.id">{{ itemList.nm }}</li>
                     </ul>
                 </div>	
             </div>
         </div>
         <div class="city_index">
             <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>D</li>
-                <li>E</li>
+                <li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{ item.index }}</li>
             </ul>
         </div>
     </div>
-</div>
 </template>
 
 <script>
-export default {
-    name:'City'
-}
+    export default {
+        name : 'City',
+        data(){
+            return {
+                cityList : [],
+                hotList : [],
+            }
+        },
+        mounted(){
+            this.axios.get('/api/cityList').then((res)=>{
+                var msg = res.data.msg;
+                if(msg === 'ok'){
+                    var cities = res.data.data.cities;
+                    //[ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                    var { cityList , hotList } = this.formatCityList(cities);
+                    this.cityList = cityList;
+                    this.hotList = hotList;
+                }
+            });
+        },
+        methods : {
+
+            // 1、首先 mounted 拿到的是一堆数据形如下
+            // 	"cities":[{"id":1,"nm":"北京","isHot":1,"py":"beijing"},{"id":10,"nm":"上海","isHot":1,"py":"shanghai"}]
+
+            // 2、循环 cities 找出热点城市 cities[i].isHot
+
+            // 3、根据 cities 自己做一个数组，即 "py":"beijing"    找第一个字母 根据字母 push 到 cityList 各个对象数组
+            // 	cityList 形如  [ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+
+            // 4、 cityList 里面的数组根据 index   abcdefg  字母排序
+
+            formatCityList(cities){
+                var cityList = [];  // 城市列表 形如  [ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                var hotList = [];   // 热门城市
+                // 循环找出热点城市
+                for(var i=0;i<cities.length;i++){
+                    if(cities[i].isHot === 1){
+                        hotList.push( cities[i] );
+                    }
+                }
+                // 找第一个字母 根据字母 push 到 cityList 各个对象数组
+                //[ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                for(var i=0;i<cities.length;i++){
+                    var firstLetter = cities[i].py.substring(0,1).toUpperCase();
+                    if(toCom(firstLetter)){  //没匹配上新添加index
+                        cityList.push({ index : firstLetter , list : [ { nm : cities[i].nm , id : cities[i].id } ] });
+                    }
+                    else{   //匹配上就累加到已有index中
+                        for(var j=0;j<cityList.length;j++){
+                            if( cityList[j].index === firstLetter ){
+                                cityList[j].list.push( { nm : cities[i].nm , id : cities[i].id } );
+                            }
+                        }
+                    }
+                }				           
+                // 判断这个字母在不在这个结果集当中   如 beijing  在不在 B 结果集当中
+                //[ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                function toCom(firstLetter){
+                    for(var i=0;i<cityList.length;i++){
+                        if( cityList[i].index ===  firstLetter){
+                            return false;   // 匹配上就往这里添加进来这个结果集中   看前面调用这个函数 toCom()
+                        }
+                    }
+                    return true;   // 没匹配上就通过了就不添加进去这个结果集
+                }
+                // 排序，让 cityList 数组对象从 a--z
+                //[ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                cityList.sort((n1,n2)=>{
+                    if( n1.index > n2.index ){
+                        return 1;
+                    }
+                    else if(n1.index < n2.index){
+                        return -1;
+                    }
+                    else{
+                        return 0;
+                    }
+                });
+                // 最后返回出去
+                return {
+                    cityList,
+                    hotList
+                };
+
+            },
+            // 点击右边的 abcd 跳转到相应的城市区域,就是 scroll 自动往上滑到顶部
+            handleToIndex(index){
+                var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+                this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+                // this.$refs.city_List.toScrollTop(-h2[index].offsetTop);
+            },
+        }
+    }
 </script>
 
-<style scoped >
+<style scoped>
     #content .city_body{ margin-top: 45px; display: flex; width:100%; position: absolute; top: 0; bottom: 0;}
     .city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
     .city_body .city_list::-webkit-scrollbar{
@@ -100,9 +130,7 @@ export default {
     }
     .city_body .city_hot{ margin-top: 20px;}
     .city_body .city_hot h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
-    .city_body .city_hot ul{}
     .city_body .city_hot ul li{ float: left; background: #fff; width: 29%; height: 33px; margin-top: 15px; margin-left: 3%; padding: 0 4px; border: 1px solid #e6e6e6; border-radius: 3px; line-height: 33px; text-align: center; box-sizing: border-box;}
-    .city_body .city_sort{}
     .city_body .city_sort div{ margin-top: 20px;}
     .city_body .city_sort h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
     .city_body .city_sort ul{ padding-left: 10px; margin-top: 10px;}
